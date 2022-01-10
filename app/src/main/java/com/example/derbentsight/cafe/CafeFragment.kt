e7db7d71a8e75a92298c77b34af24ba7ee45dab5
Samim <Samim058@gamil.com>
2022-01-10 18:28:40 +0300

    Start, add fragments and cardView

---
+++ app/src/main/java/com/example/derbentsight/cafe/CafeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.derbentsight.cafe
+
+import androidx.fragment.app.Fragment
+
+class CafeFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/example/derbentsight/sight/SightFragment.kt
@@ -0,0 +1,7 @@
+package com.example.derbentsight.sight
+
+import androidx.fragment.app.Fragment
+
+class SightFragment : Fragment() {
+
+}
