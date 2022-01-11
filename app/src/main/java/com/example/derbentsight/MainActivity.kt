8bff44e9ee1c466d6a6f01df003eac219d76a79e
Samim <Samim058@gamil.com>
2022-01-12 00:39:14 +0300

    Start, add tabLayout

--- app/src/main/java/com/example/derbentsight/MainActivity.kt
+++ app/src/main/java/com/example/derbentsight/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.derbentsight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.derbentsight.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
---
+++ app/src/main/java/com/example/derbentsight/adapterTab/AdapterTab.kt
@@ -0,0 +1,12 @@
+package com.example.derbentsight.adapterTab
+
+import androidx.viewpager.widget.PagerAdapter
+
+/*class AdapterTab: PagerAdapter() {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+
+
+    }
+
+}*/
